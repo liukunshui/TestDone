316c11b010fd669d60c03981c18a5001f9528645
liukunshui <272795906@qq.com>
2016-12-29 16:00:33 +0800

    舒服的

---
+++ KSFDFullscreenPopGesture/KS_OneViewController.h
@@ -0,0 +1,13 @@
+//
+//  KS_OneViewController.h
+//  KSFDFullscreenPopGesture
+//
+//  Created by liuks on 16/12/29.
+//  Copyright © 2016年 wacosoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KS_OneViewController : UIViewController
+
+@end
--- KSFDFullscreenPopGesture/KS_OneViewController.m
+++ KSFDFullscreenPopGesture/KS_OneViewController.m
@@ -1,44 +1,53 @@
 //
-//  ViewController.m
+//  KS_OneViewController.m
 //  KSFDFullscreenPopGesture
 //
 //  Created by liuks on 16/12/29.
 //  Copyright © 2016年 wacosoft. All rights reserved.
 //
 
-#import "ViewController.h"
 #import "KS_OneViewController.h"
-#import "UINavigationController+FDFullscreenPopGesture.h"
-@interface ViewController ()
+
+@interface KS_OneViewController ()
 
 @end
 
-@implementation ViewController
+@implementation KS_OneViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-//    self.navigationController.fd_prefersNavigationBarHidden = YES;
+    
     UIButton *btn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
     btn.backgroundColor = [UIColor lightGrayColor];
-    [btn setTitle:@"Push" forState:UIControlStateNormal];
+    [btn setTitle:@"One" forState:UIControlStateNormal];
     btn.frame = CGRectMake(10,70+80, 80,40);
     [btn addTarget:self action:@selector(go) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:btn];
 
     
-    // Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view from its nib.
 }
 -(void)viewWillAppear:(BOOL)animated{
     [super viewWillAppear:YES];
     self.navigationController.navigationBarHidden = YES;
 }
 -(void)go{
-    KS_OneViewController *one =[[KS_OneViewController alloc]init];
-    [self.navigationController pushViewController:one animated:YES];
+    [self.navigationController popViewControllerAnimated:YES];
 }
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
 
 @end
